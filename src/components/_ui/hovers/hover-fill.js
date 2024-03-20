a6c0af73e89be16beb1e445180ed53fb5b00e92b
Dany Atanov <Dany Atanov>
2024-03-20 14:00:52 +0300

    Добавлены новые компоненты

---
+++ src/components/_ui/hovers/hover-fill.js
@@ -0,0 +1,36 @@
+const showMore = (() => {
+	let hoverElements = document.querySelectorAll('.hover-fill');
+
+	let init = () => {
+		if (hoverElements.length === 0) return;
+
+		const ripple = document.createElement('span');
+		ripple.classList.add('--ripple');
+		let cleartimeout;
+
+		hoverElements.forEach((element) => {
+			element.addEventListener('mouseover', function (event) {
+				const boundingClientRect = event.target.getBoundingClientRect();
+				let x = event.clientX - boundingClientRect.left;
+				let y = event.clientY - boundingClientRect.top;
+				ripple.style.left = x + 'px';
+				ripple.style.top = y + 'px';
+				this.appendChild(ripple);
+			});
+
+			element.addEventListener('mouseout', function () {
+				ripple.remove(cleartimeout);
+			});
+
+			cleartimeout = setTimeout(() => {
+				ripple.remove();
+			}, 1000);
+		});
+
+		_setupListeners();
+	};
+
+	let _setupListeners = () => {};
+
+	init();
+})();
