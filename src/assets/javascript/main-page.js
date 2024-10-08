6d0d56a7f69f5ec9a6aace76a5ab903ec27f64c2
Dany Atanov <Dany Atanov>
2024-10-08 16:18:39 +0300

    fix: цвет активной ссылки подменю теперь окрашивается

--- src/assets/javascript/main-page.js
+++ src/assets/javascript/main-page.js
@@ -1,1 +1,21 @@
+const $chess = document.createElement('table');
+$chess.classList.add('chess');
+document.querySelector('.layout__content').appendChild($chess);
 
+const $row = document.createElement('tr');
+const $cell = document.createElement('td');
+
+$cell.style.width = '50px;';
+$cell.style.height = '50px;';
+
+for (let i = 0; i < 8; i++) {
+	let newRow = $chess.appendChild($row.cloneNode(true));
+
+	for (let j = 0; j < 8; j++) {
+		if (j % 2 === 0) {
+			newRow.appendChild($cell.cloneNode(true)).classList.add('black');
+		} else {
+			newRow.appendChild($cell.cloneNode(true));
+		}
+	}
+}
